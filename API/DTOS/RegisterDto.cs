20b7d014825f9dba641790aaacec282f67e1a160
NguyenAnhDuc42 <144120505+NguyenAnhDuc42@users.noreply.github.com>
2024-08-26 20:26:37 +0700

    end of section 7

--- API/DTOS/RegisterDto.cs
+++ API/DTOS/RegisterDto.cs
@@ -1,10 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOS;
 
 public class RegisterDto
 {
-    public required string Username { get; set; }
-    public required string Password { get; set; }
+    [Required]
+    public  string Username { get; set; }
+    [Required]
+    [StringLength(8,MinimumLength =3)]
+    public  string Password { get; set; }
 
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,28 +3,33 @@ import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule],
+  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService =inject(AccountService)
-
+  private router = inject(Router)
+  private toastr = inject(ToastrService)
   model : any ={};
   Login() {
    this.accountService.login(this.model).subscribe({
-     next : response =>{
-      console.log(response);
+     next : () =>{
+      this.router.navigateByUrl('/members')
      },
-     error : error => console.log(error)
+     error : error => this.toastr.error(error.error)
    })
   }
 
   Logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 }
