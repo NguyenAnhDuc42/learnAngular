7a08c3c42e6ec4c424a38d9aa9063910826de55a
NguyenAnhDuc42 <144120505+NguyenAnhDuc42@users.noreply.github.com>
2024-09-01 10:05:04 +0700

    end of section 9.0

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,25 +8,25 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
-  http = inject(HttpClient);
+export class HomeComponent {
+  // http = inject(HttpClient);
   registerMode = false;
-  user : any;
+  // user : any;
 
-  ngOnInit(): void {
-    this.getUser()
-  }
+  // ngOnInit(): void {
+  //   this.getUser()
+  // }
   registerToggle(){
     this.registerMode = !this.registerMode
   }
   cancelRegisterMode(event:boolean){
     this.registerMode = event
   }
-  getUser(){
-    this.http.get('http://localhost:5001/api/user').subscribe({
-      next: responce => this.user = responce,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    })
-  }
+  // getUser(){
+  //   this.http.get('http://localhost:5001/api/user').subscribe({
+  //     next: responce => this.user = responce,
+  //     error: error => console.log(error),
+  //     complete: () => console.log('complete')
+  //   })
+  // }
 }
---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import { APP_ID } from "@angular/core";
+
+export const environment = {
+    production:false,
+    apiUrl:'http://localhost:5001/api/'
+};
